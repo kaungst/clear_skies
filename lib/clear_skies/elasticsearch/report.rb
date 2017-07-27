c4fbc6a5ecd230017875ecd0af58ea5be1b29242
cconstantine <chris@omadahealth.com>
2017-07-27 15:24:26 -0700

    [#147302953] Add node metrics to elasticsearch

--- lib/clear_skies/elasticsearch/report.rb
+++ lib/clear_skies/elasticsearch/report.rb
@@ -40,66 +40,44 @@ module ClearSkies
         ClearSkies::Elasticsearch::Report.flatten_hash(stats)
       end
 
-      def dimensions
+      def indicies
         client = ::Elasticsearch::Client.new(hosts: @url)
         client.indices.get_aliases
       end
 
-      def report_dimensions
-        dimensions.map do |dimension|
-          ReportDimension.new(self, dimension)
+      def report_indicies
+        indicies.map do |index|
+          ReportIndex.new(self, index)
+        end
+      end
+
+      def nodes
+        client = ::Elasticsearch::Client.new(hosts: @url)
+        client.nodes.stats["nodes"]
+      end
+
+      def report_nodes
+        nodes.map do |node_name, node_props|
+          ReportNode.new(self, node_name, node_props)
         end
       end
     end
 
-    class MeasureSet < GreekFire::MeasureSet
-      def items
-        metrics = []
+    class ReportIndex
+      attr_reader :report, :index
 
-        labels = ClearSkies::Elasticsearch::Report.reports.map do | report|
-          GreekFire::SmartLabel.new(report.metrics, report.extra_labels)
-        end
-
-        metric_names = labels.map {|i| i.value.keys}.flatten.uniq
-
-        metric_names.each do |metric|
-          metrics << GreekFire::Gauge.new(metric, labels: labels) do |label|
-            label.value[metric]
-          end
-        end
-
-        labels = ClearSkies::Elasticsearch::Report.reports.map do | report|
-          report.report_dimensions.map do |report_dimension|
-            GreekFire::SmartLabel.new(report_dimension.metrics, report_dimension.extra_labels)
-          end
-        end.flatten
-
-        metric_names = labels.map {|i| i.value.keys}.flatten.uniq
-
-        metric_names.each do |metric|
-          metrics << GreekFire::Gauge.new(metric, labels: labels) do |label|
-            label.value[metric]
-          end
-        end
-        metrics
-      end
-    end
-
-    class ReportDimension
-      attr_reader :report, :dimension, :extra_labels
-
-      def initialize(report, dimension)
+      def initialize(report, index)
         @report = report
-        @dimension = dimension
-        @extra_labels = { index: index_alias(dimension) }
+        @index = index
+        @extra_labels = { index: index_alias }
       end
 
-      def index_alias(dimension)
-        @dimension[1]["aliases"].keys&.first || @dimension[0]
+      def index_alias
+        @index[1]["aliases"].keys&.first || @index[0]
       end
 
       def index_name
-        @dimension[0]
+        @index[0]
       end
 
       def extra_labels
@@ -113,6 +91,78 @@ module ClearSkies
         ClearSkies::Elasticsearch::Report.flatten_hash(stats)
       end
     end
+
+    class ReportNode
+      attr_reader :report, :extra_labels
+
+      def initialize(report, node_name, node_props)
+        @report = report
+        @node_props = node_props
+        @extra_labels = { node_name: node_name }
+      end
+
+      def extra_labels
+        @report.extra_labels.merge(@extra_labels)
+      end
+
+      def metrics
+        ClearSkies::Elasticsearch::Report.flatten_hash(@node_props, "elasticsearch_node")
+      end
+
+    end
+
+
+    class MeasureSet < GreekFire::MeasureSet
+      def items
+        metrics = []
+
+        ### Cluster Metrics ##
+        labels = ClearSkies::Elasticsearch::Report.reports.map do | report|
+          GreekFire::SmartLabel.new(report.metrics, report.extra_labels)
+        end
+
+        metric_names = labels.map {|i| i.value.keys}.flatten.uniq
+
+        metric_names.each do |metric|
+          metrics << GreekFire::Gauge.new(metric, labels: labels) do |label|
+            label.value[metric]
+          end
+        end
+
+        ### Index Metrics
+        labels = ClearSkies::Elasticsearch::Report.reports.map do | report|
+          report.report_indicies.map do |report_index|
+            GreekFire::SmartLabel.new(report_index.metrics, report_index.extra_labels)
+          end
+        end.flatten
+
+        metric_names = labels.map {|i| i.value.keys}.flatten.uniq
+
+        metric_names.each do |metric|
+          metrics << GreekFire::Gauge.new(metric, labels: labels) do |label|
+            label.value[metric]
+          end
+        end
+
+        ### Node Metrics
+        labels = ClearSkies::Elasticsearch::Report.reports.map do | report|
+          report.report_nodes.map do |report_node|
+            GreekFire::SmartLabel.new(report_node.metrics, report_node.extra_labels)
+          end
+        end.flatten
+
+        metric_names = labels.map {|i| i.value.keys}.flatten.uniq
+
+        metric_names.each do |metric|
+          metrics << GreekFire::Gauge.new(metric, labels: labels) do |label|
+            label.value[metric]
+          end
+        end
+
+        metrics
+      end
+    end
+
   end
 end
 
